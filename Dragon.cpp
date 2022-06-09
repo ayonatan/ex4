31287b678b764e5cfde3c7c26a4b00be260a14fe
ayonatan <102904006+ayonatan@users.noreply.github.com>
2022-06-09 21:10:47 +0300

    Update Dragon.cpp

--- Dragon.cpp
+++ Dragon.cpp
@@ -1,5 +1,21 @@
-//
-// Created by yonat on 06/06/2022.
-//
-
 #include "Dragon.h"
+
+Dragon::Dragon(const char* m_name) : Player(const char* m_name),m_force(25), m_loot(1000), m_damage(100) {}
+
+
+void Dragon::applyEncounter(Player &player) const {
+    if (player.getAttackStrength() >= m_force) {
+        player.addCoins(m_loot);
+        player.levelUp();
+
+       /// printBattleResult(true);
+    } else {
+        player.damage(m_damage);
+       /// printBattleResult(false);
+    }
+}
+
+std::ostream &operator<<(std::ostream &os, const Goblin &goblin) {
+    printMonsterDetails(std, m_force, m_damage, m_loot, true);
+    return os;
+}
