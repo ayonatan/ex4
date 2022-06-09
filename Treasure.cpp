01a7a258a6b898040280927ec87c0e236e0368ec
ayonatan <102904006+ayonatan@users.noreply.github.com>
2022-06-09 21:24:28 +0300

    Update Treasure.cpp

--- Treasure.cpp
+++ Treasure.cpp
@@ -1,5 +1,8 @@
-//
-// Created by yonat on 06/06/2022.
-//
-
 #include "Treasure.h"
+
+void Treasure::applyEncounter(Player &player) const {
+    //check with michal seif 3.7
+    //magic  number
+    player.addCoins(10);
+    printTreasureMessage();
+}
