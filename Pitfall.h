7eb78c53bb005270c5890437dc92c285d11bfa00
ayonatan <102904006+ayonatan@users.noreply.github.com>
2022-06-09 21:12:59 +0300

    Update Pitfall.h

--- Pitfall.h
+++ Pitfall.h
@@ -1,8 +1,9 @@
-//
-// Created by yonat on 06/06/2022.
-//
-
 #ifndef MTM_HW4_PITFALL_H
 #define MTM_HW4_PITFALL_H
+#include "Card.h"
 
+class PitFall : public Card{
+public:
+    void applyEncounter(Player& player)const override;
+};
 #endif //MTM_HW4_PITFALL_H
