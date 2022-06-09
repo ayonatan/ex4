c3cbba711abd0b296d8456766afba67de81ebc0e
ayonatan <102904006+ayonatan@users.noreply.github.com>
2022-06-09 21:09:02 +0300

    Update Goblin.h

--- Goblin.h
+++ Goblin.h
@@ -1,8 +1,21 @@
-//
-// Created by yonat on 06/06/2022.
-//
-
 #ifndef MTM_HW4_GOBLIN_H
 #define MTM_HW4_GOBLIN_H
+
+#include "Player.h"
+
+class Goblin : public Player {
+public:
+    Goblin(const char* m_name);
+
+    Goblin(const Goblin &goblin) = default;
+
+    ~Goblin() =default override;
+
+    operator=(const Goblin &goblin) = default;
+    void applyEncounter(Player& player)const override;
+
+///private: add?
+
+};
 
 #endif //MTM_HW4_GOBLIN_H
