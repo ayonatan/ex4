e2008cfeb035d4139eeb7d2079aee127b7d51a77
ayonatan <102904006+ayonatan@users.noreply.github.com>
2022-06-09 21:25:21 +0300

    Update Vampire.cpp

--- Vampire.cpp
+++ Vampire.cpp
@@ -1,5 +1,21 @@
-//
-// Created by yonat on 06/06/2022.
-//
-
 #include "Vampire.h"
+
+Vampire::Vampire(const char* m_name) : Player(const char* m_name),m_force(10), m_loot(2), m_damage(10) {}
+
+
+void Vampire::applyEncounter(Player &player) const {
+    if (player.getAttackStrength() >= m_force) {
+        player.addCoins(m_loot);
+        player.levelUp();
+
+        /// printBattleResult(true);
+    } else {
+        player.damage(m_damage);
+        /// printBattleResult(false);
+    }
+}
+
+std::ostream &operator<<(std::ostream &os, const Goblin &goblin) {
+    printMonsterDetails(std, m_force, m_damage, m_loot, true);
+    return os;
+}
