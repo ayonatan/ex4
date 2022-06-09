1a825d68cdba764da2816927cbb4bd7cc6bc1822
ayonatan <102904006+ayonatan@users.noreply.github.com>
2022-06-09 21:25:00 +0300

    Update Treasure.h

--- Treasure.h
+++ Treasure.h
@@ -1,10 +1,13 @@
 #ifndef MTM_HW4_TREASURE_H
 #define MTM_HW4_TREASURE_H
+
 #include "Card.h"
+
 class Treasure : public Card {
-
-
+public:
+    void applyEncounter(Player &player) const override;
 };
+
 
 
 #endif //MTM_HW4_TREASURE_H
