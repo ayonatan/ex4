af0bd3cb1da2d26026e64b4051d3d1a103c09a24
ayonatan <102904006+ayonatan@users.noreply.github.com>
2022-06-09 21:25:35 +0300

    Update Vampire.h

--- Vampire.h
+++ Vampire.h
@@ -1,8 +1,23 @@
-//
-// Created by yonat on 06/06/2022.
-//
-
 #ifndef MTM_HW4_VAMPIRE_H
 #define MTM_HW4_VAMPIRE_H
+
+#include "Player.h"
+
+class Vampire : public Player {
+public:
+    Vampire(const char* m_name);
+
+    Vampire(const Vampire &vampire) = default;
+
+    ~Dragon() = default override;
+
+    operator=(const Vampire &vampire) = default;
+
+    void applyEncounter(Player &player) const override;
+
+///private: add?
+
+
+#endif //MTM_HW4_DRAGON_H
 
 #endif //MTM_HW4_VAMPIRE_H
