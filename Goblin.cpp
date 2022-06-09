c2764803a3b5c5a057ca504e2f90049eddf5c87b
ayonatan <102904006+ayonatan@users.noreply.github.com>
2022-06-09 21:08:28 +0300

    Update Goblin.cpp

--- Goblin.cpp
+++ Goblin.cpp
@@ -1,5 +1,21 @@
-//
-// Created by yonat on 06/06/2022.
-//
-
 #include "Goblin.h"
+
+Goblin::Goblin(const char* m_name) : Player(const char* m_name),m_force(6), m_loot(2), m_damage(10) {}
+
+
+void Goblin::applyEncounter(Player &player) const {
+    if (player.getAttackStrength() >= m_force) {
+        player.addCoins(m_loot);
+        player.levelUp();
+
+      ///  printBattleResult(true);
+    } else {
+        player.damage(m_damage);
+      ///  printBattleResult(false);
+    }
+}
+
+std::ostream &operator<<(std::ostream &os, const Goblin &goblin) {
+    printMonsterDetails(std, m_force, m_damage, m_loot, false);
+    return os;
+}
