e79f2a9c3298ee47282fb0201235260031cc0629
ayonatan <102904006+ayonatan@users.noreply.github.com>
2022-06-09 21:11:40 +0300

    Update Fairy.h

--- Fairy.h
+++ Fairy.h
@@ -1,8 +1,12 @@
-//
-// Created by yonat on 06/06/2022.
-//
 
 #ifndef MTM_HW4_FAIRY_H
 #define MTM_HW4_FAIRY_H
+
+#include "Card.h"
+
+class Fairy : public Card {
+public:
+    void applyEncounter(Player &player) const override;
+};
 
 #endif //MTM_HW4_FAIRY_H
