04f1f73503e9ebbd46fb94436d0361059d80c559
ayonatan <102904006+ayonatan@users.noreply.github.com>
2022-06-09 21:11:02 +0300

    Update Dragon.h

--- Dragon.h
+++ Dragon.h
@@ -1,8 +1,21 @@
-//
-// Created by yonat on 06/06/2022.
-//
-
 #ifndef MTM_HW4_DRAGON_H
 #define MTM_HW4_DRAGON_H
+
+#include "Player.h"
+
+class Dragon : public Player {
+public:
+    Dragon(const char* m_name);
+
+    Dragon(const Dragon &dragon) = default;
+
+    ~Dragon() = default override;
+
+    operator=(const Dragon &dragon) = default;
+
+    void applyEncounter(Player &player) const override;
+
+///private: add?
+
 
 #endif //MTM_HW4_DRAGON_H
