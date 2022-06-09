31d2faae5ab3d774f1a12b5ee8d0d6f26d22dc78
ayonatan <102904006+ayonatan@users.noreply.github.com>
2022-06-09 21:09:34 +0300

    Update Barfight.h

--- Barfight.h
+++ Barfight.h
@@ -1,8 +1,10 @@
-//
-// Created by yonat on 06/06/2022.
-//
-
 #ifndef MTM_HW4_BARFIGHT_H
 #define MTM_HW4_BARFIGHT_H
+#include "Card.h"
 
+class Barfight : public Card{
+public:
+    void applyEncounter(Player& player)const override;
+};
 #endif //MTM_HW4_BARFIGHT_H
+
