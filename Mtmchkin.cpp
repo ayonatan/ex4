7d6141024ff6f3a0586c4a6ee7249d2030011a87
ayonatan <102904006+ayonatan@users.noreply.github.com>
2022-06-16 21:21:49 +0300

    Update Mtmchkin.cpp

--- Mtmchkin.cpp
+++ Mtmchkin.cpp
@@ -1,5 +1,7 @@
 #include "Mtmchkin.h"
+#include <vector>
 
+/*
 Mtmchkin::Mtmchkin(const char* playerName, const Card* cardsArray, int numOfCards) :
         m_player(playerName),
         m_cards_array(new Card[numOfCards]),
@@ -11,13 +13,11 @@ Mtmchkin::Mtmchkin(const char* playerName, const Card* cardsArray, int numOfCard
     {
         m_cards_array[i] = cardsArray[i];
     }
-}
+}*/
 
-Mtmchkin::~Mtmchkin()
-{
-    delete[] m_cards_array;
-}
 
+
+/*
 Mtmchkin::Mtmchkin(const Mtmchkin &game):
         m_player(game.m_player),
         m_cards_array(new Card[game.m_num_of_cards]),
@@ -29,59 +29,79 @@ Mtmchkin::Mtmchkin(const Mtmchkin &game):
     {
         m_cards_array[i] = game.m_cards_array[i];
     }
-}
+}*/
 
-Mtmchkin& Mtmchkin::operator=(const Mtmchkin &game)
-{
-    if (this == &game)
-    {
+Mtmchkin &Mtmchkin::operator=(const Mtmchkin &game) {
+    if (this == &game) {
         return *this;
     }
     m_player = game.m_player;
     delete[] m_cards_array;
     m_cards_array = new Card[game.m_num_of_cards];
-    for(int i = 0; i < m_num_of_cards; i++)
-    {
+    for (int i = 0; i < m_num_of_cards; i++) {
         m_cards_array[i] = game.m_cards_array[i];
     }
     m_num_of_cards = game.m_num_of_cards;
     m_game_status = game.m_game_status;
-    m_round=game.m_round;
+    m_round = game.m_round;
     return *this;
 }
 
-void Mtmchkin::playNextCard()
-{
-    if (m_game_status != GameStatus::MidGame)
-    {
+void Mtmchkin::playNextCard() {
+    if (m_game_status != GameStatus::MidGame) {
         return;
     }
     m_cards_array[m_round].printInfo();
     m_cards_array[m_round].applyEncounter(m_player);
     m_player.printInfo();
     m_round++;
-    if (m_round == m_num_of_cards)
-    {
+    if (m_round == m_num_of_cards) {
         m_round = 0;
     }
-    if (m_player.getLevel() == 10)
-    {
-        m_game_status=GameStatus::Win;
+    if (m_player.getLevel() == 10) {
+        m_game_status = GameStatus::Win;
         return;
     }
-    if (m_player.isKnockedOut())
-    {
-        m_game_status=GameStatus::Loss;
+    if (m_player.isKnockedOut()) {
+        m_game_status = GameStatus::Loss;
         return;
     }
 }
 
-GameStatus Mtmchkin::getGameStatus()const
-{
+GameStatus Mtmchkin::getGameStatus() const {
     return m_game_status;
 }
 
-bool Mtmchkin::isOver()const
-{
-    return m_game_status!=GameStatus::MidGame;
+bool Mtmchkin::isOver() const {
+    return m_game_status != GameStatus::MidGame;
+}
+
+
+Mtmchkin::Mtmchkin(const std::string fileName) {
+
+    ifstream source(fileName);
+    int i = 0;
+    std::string cardName;
+    while (std::getline(source, cardName)) {
+        switch (cardName) {
+            case "Fairy":
+                m_cardsDeck.push_back(new Fairy());
+            case "Goblin":
+                m_cardsDeck.push_back(new Goblin());
+            case "Vampire":
+                m_cardDeck.push_back(new Vampire());
+            case "BarFight":
+                m_cardDeck.push_back(new BarFight());
+            case "Dragon":
+                m_cardDeck.push_back(new Dragon());
+            case "Treasure":
+                m_cardDeck.push_back(new Treasure());
+            case "Merchant":
+                m_cardDeck.push_back(new Merchant());
+        }
+    }
+}
+
+void playRound() {
+
 }
