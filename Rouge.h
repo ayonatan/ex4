fdfb8367a4854690cf6c4b7262cdd9caf4cbd8b2
ayonatan <102904006+ayonatan@users.noreply.github.com>
2022-06-16 21:24:30 +0300

    Update Rouge.h

--- Rouge.h
+++ Rouge.h
@@ -4,19 +4,24 @@
 
 #ifndef MTM_HW4_ROUGE_H
 #define MTM_HW4_ROUGE_H
+
 #include "Player.h"
 
-class Rouge : public Player
-{
+class Rouge : public Player {
 public:
-    Rouge(std::string name) = default;
-    ~Rouge() = default;
-    Rouge(const Rouge &rouge) = delete;
-//    Rouge &operator=(const Rouge &rouge) = default;
+    Rouge(std::string name);
 
-    void addCoins(int coinsToAdd)override;
+    ~Rouge() = default;
+
+    Rouge(const Rouge &rouge) = delete;
+
+    Rouge &operator=(const Rouge &rouge) = default; ////?
+
+    void addCoins(int coinsToAdd) override;
+
     void printInfo() const override;
-    Rouge* clone() const override;
+
+    Rouge *clone() const override;
 };
 
 #endif //MTM_HW4_ROUGE_H
