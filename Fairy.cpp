f2a0906159eb3a352b493b5379b21c4d650aa2d2
ayonatan <102904006+ayonatan@users.noreply.github.com>
2022-06-16 21:19:27 +0300

    Update Fairy.cpp

--- Fairy.cpp
+++ Fairy.cpp
@@ -1,5 +1,9 @@
 #include "Fairy.h"
 
+Fairy::Fairy() : Card() {
+    m_cardName = "Fairy";
+}
+
 void Fairy::applyEncounter(Player &player) const {
     const Wizard *wizard = dynamic_cast<const Wizard *>(wizard);
     if (wizard != nullptr) {
@@ -10,9 +14,10 @@ void Fairy::applyEncounter(Player &player) const {
 }
 
 void printInfo() const {
-    printCardDetails(std::cout,"Fairy");
+    printCardDetails(std::cout, "Fairy");
 }
 
 Fairy *clone() const {
     return new Fairy(m_name);
 }
+
