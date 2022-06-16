6156bedae008cf7f3f2da36fdd16ea024906b104
ayonatan <102904006+ayonatan@users.noreply.github.com>
2022-06-16 21:21:12 +0300

    Update Merchant.cpp

--- Merchant.cpp
+++ Merchant.cpp
@@ -1,24 +1,31 @@
 #include "Merchant.h"
 
+Merchant::Merchant()
+{
+    m_cardName = "Merchant";
+}
+
 void Merchant::applyEncounter(Player &player) const {
     // bool flag = true;
     int payIndex;
     while (true) {
         printMerchantInitialMessageForInteractiveEncounter(std, player.getName(), player.getCoins());
         // can we use magic numbers
-        if (std::cin >> payIndex != 0 && std::cin >> payIndex != 1 && std::cin >> payIndex != 2) {
+        std::cin >> payIndex;
+        if (payIndex != 0 && payIndex != 1 && payIndex != 2) {
             printInvalidInput();
             continue;
         }
         if (!player.pay()) {
             return;
-            printMerchantSummary(std,player.getName(),player.getCoins());
+            printMerchantSummary(std, player.getName(), player.getCoins());
         }
     }
-    printMerchantSummary(std,player.getName(),player.getCoins());
+    printMerchantSummary(std, player.getName(), player.getCoins());
 }
+
 void printInfo() const {
-    printCardDetails(std::cout,"Merchant");
+    printCardDetails(std::cout, "Merchant");
 }
 
 Merchant *clone() const {
