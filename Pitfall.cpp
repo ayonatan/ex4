a6434a42e5a1d7fdf0e0160fc23646195ba21f47
ayonatan <102904006+ayonatan@users.noreply.github.com>
2022-06-16 21:22:15 +0300

    Update Pitfall.cpp

--- Pitfall.cpp
+++ Pitfall.cpp
@@ -1,5 +1,10 @@
 #include "Pitfall.h"
 
+PitFall::PitFall()
+{
+    m_cardName = "BarFight";
+}
+
 void PitFall::applyEncounter(Player &player) const {
     const Rouge *rouge = dynamic_cast<const Rouge *>(player);
     if (rouge == nullptr) {
